b56c59c6b844fddbc3e3118bcfc7cdf8bf5ca59f
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2021-12-09 22:12:44 +0530

    Added basic multiprocessor functionality

--- misc/script.py
+++ misc/script.py
@@ -4,11 +4,19 @@
 Example of a script that uses SimSo.
 """
 
+import re
+
 import sys
 from simso.core import Model
 from simso.configuration import Configuration
 
-
+schedulers = [
+"simso.schedulers.RM",
+"simso.schedulers.LB_P_EDF",
+"simso.schedulers.P_EDF2",
+"simso.schedulers.EDHS",
+"simso.schedulers.P_EDF",
+]
 def main(argv):
     if len(argv) == 2:
         # Configuration load from a file.
@@ -20,19 +28,25 @@ def main(argv):
         configuration.duration = 420 * configuration.cycles_per_ms
 
         # Add tasks:
-        configuration.add_task(name="T1", identifier=1, period=7,
-                               activation_date=0, wcet=3, deadline=7)
-        configuration.add_task(name="T2", identifier=2, period=12,
-                               activation_date=0, wcet=3, deadline=12)
-        configuration.add_task(name="T3", identifier=3, period=20,
-                               activation_date=0, wcet=5, deadline=20)
+        n = 12
+        for i in range(n):
+            taskName = "T" + str(i)
+            configuration.add_task(name=taskName, identifier=i, period=20,activation_date=0, wcet=3, deadline=20)
+
 
         # Add a processor:
         configuration.add_processor(name="CPU 1", identifier=1)
-
+        configuration.add_processor(name="CPU 2", identifier=2)
+        configuration.add_processor(name="CPU 3", identifier=3)
+        # configuration.add_processor(name="CPU 4", identifier=4)
+        # configuration.add_processor(name="CPU 5", identifier=5)
+        # configuration.add_processor(name="CPU 6", identifier=6)
+        # configuration.add_processor(name="CPU 7", identifier=7)
         # Add a scheduler:
-        #configuration.scheduler_info.filename = "../simso/schedulers/RM.py"
+
+        # configuration.scheduler_info.filename = "../simso/schedulers/RM.py"
         configuration.scheduler_info.clas = "simso.schedulers.RM"
+        
 
     # Check the config before trying to run it.
     configuration.check_all()
